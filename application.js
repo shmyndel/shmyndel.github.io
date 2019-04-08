70500220fcc7eaecf40f4edd523e135d0ce6eb9c
shmyndel <shmyndel@gmail.com>
2019-04-08 22:45:07 +0300

    Add files via upload

--- application.js
+++ application.js
@@ -1,8 +1,24 @@
 const body = document.body;
-body.style.transitionTimingFunction = "linear";
-		
+const blured = document.querySelectorAll('.blur');
+const links = document.querySelectorAll('a');
+
 body.addEventListener('mousemove',(e) => {
 	var x = e.offsetX;
 	var y = e.offsetY;
 	body.style.background = `hsl(${x}, ${y}%, 80%)`;
 });
+
+links.forEach(link => link.addEventListener('mouseover',(e) => {
+	blured.forEach(e => e.style.filter = 'blur(8px)');
+	e.currentTarget.style.filter = 'blur(0px)';
+}));
+
+links.forEach(link => link.addEventListener('mouseout',(e) => {
+	blured.forEach(e => e.style.filter = 'blur(0px)');
+	e.currentTarget.style.filter = 'blur(0px)';
+}));
+
+var today = new Date();
+var yyyy = today.getFullYear();
+
+document.querySelector('.currentYear').innerText = yyyy;
